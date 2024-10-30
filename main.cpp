759d2d52dc6cf10da3270a0e7cb1f28646bc7dc4
DOCUTEE <110187691+DOCUTEE@users.noreply.github.com>
2024-10-30 12:35:37 +0700

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define MASK(i) (1LL << (i))
+const int inf = 1e18;
+
+void solve()
+{
+      cin >> a >> b;
+}
+signed main()
+{
+      ios_base::sync_with_stdio(0);
+      cin.tie(0);
+      cout.tie(0);
+      int t = 1;
+      if (t > 1)
+            cin >> t;
+      while (t--)
+      {
+            solve();
+      }
+}
+// I will be in OLP HANOI next 6 months
